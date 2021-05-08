01194e1f377628489a815a9292f079cdd3192a8d
achrefthameur <benthameur@mail.com>
2021-05-08 05:11:18 +0100

    notifications from chat

--- database/sql/Pinder.sql
+++ database/sql/Pinder.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: May 01, 2021 at 12:08 AM
+-- Generation Time: May 08, 2021 at 04:09 AM
 -- Server version: 10.4.11-MariaDB-1:10.4.11+maria~bionic
 -- PHP Version: 7.4.16
 
@@ -20,69 +20,79 @@ SET time_zone = "+00:00";
 --
 -- Database: `Pinder`
 --
-Use Pinder;
+CREATE DATABASE IF NOT EXISTS `Pinder` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `Pinder`;
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `chat_messages`
 --
 
-CREATE TABLE IF NOT EXISTS `chat_messages` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
+CREATE TABLE  IF NOT EXISTS `chat_messages` (
+  `id` int(6) NOT NULL,
   `room_id` int(12) NOT NULL,
   `sender_id` int(6) NOT NULL,
   `receiver_id` int(6) NOT NULL,
   `message` varchar(255) NOT NULL,
-  `date` datetime NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (id)
+  `date` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sessions`
+-- Table structure for table `Friends`
 --
 
-CREATE TABLE IF NOT EXISTS `sessions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_user` int(11) NOT NULL,
-  `token` varchar(255) NOT NULL,
-  `create_At` datetime NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS `Friends` (
+  `id` int(11) NOT NULL,
+  `userOne` int(11) NOT NULL,
+  `userTwo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `invitations`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `phone_num` varchar(20) NOT NULL,
-  `e_mail` varchar(50) DEFAULT NULL,
-  `first` varchar(50) NOT NULL,
-  `last` varchar(50) DEFAULT NULL,
-  `biography` text DEFAULT NULL,
-  `creatAt` datetime DEFAULT current_timestamp(),
+CREATE TABLE  IF NOT EXISTS `invitations` (
+  `id` int(11) NOT NULL,
+  `sender` int(11) NOT NULL,
+  `receiver` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Notifications`
+--
+
+CREATE TABLE IF NOT EXISTS `Notifications` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `Friend_id` int(11) NOT NULL,
+  `content` varchar(255) NOT NULL,
   `photo` varchar(255) NOT NULL,
-  PRIMARY KEY (id)
+  `date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `pets`
 --
 
 CREATE TABLE IF NOT EXISTS `pets` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `nickname` varchar(50) NOT NULL,
+  `pet` varchar(255) NOT NULL,
   `gendre` varchar(50) NOT NULL,
   `birth` date DEFAULT NULL,
   `category` varchar(255) NOT NULL,
-  PRIMARY KEY (id)
+  `likes` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -92,31 +102,161 @@ CREATE TABLE IF NOT EXISTS `pets` (
 --
 
 CREATE TABLE IF NOT EXISTS `pets_pictures` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `pet_id` int(11) NOT NULL,
-  `picture_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (id)
-
+  `picture_name` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `create_At` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE  IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL,
+  `phone_num` varchar(20) DEFAULT NULL,
+  `e_mail` varchar(50) DEFAULT NULL,
+  `first` varchar(50) NOT NULL,
+  `last` varchar(50) DEFAULT NULL,
+  `biography` text DEFAULT NULL,
+  `creatAt` datetime DEFAULT current_timestamp(),
+  `photo` varchar(255) NOT NULL DEFAULT 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png',
+  `city` varchar(255) DEFAULT NULL,
+  `latitude` varchar(255) DEFAULT NULL,
+  `longitude` varchar(255) DEFAULT NULL,
+  `notifications_Token` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `verification_sms`
 --
+
 CREATE TABLE IF NOT EXISTS `verification_sms` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `number` varchar(14) NOT NULL,
-  `code` varchar(6) NOT NULL,
-  PRIMARY KEY (id)
+  `code` varchar(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Friends`
+--
+ALTER TABLE `Friends`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `invitations`
+--
+ALTER TABLE `invitations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Notifications`
+--
+ALTER TABLE `Notifications`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pets`
+--
+ALTER TABLE `pets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pets_pictures`
+--
+ALTER TABLE `pets_pictures`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD UNIQUE KEY `phone_num` (`phone_num`),
-  ADD UNIQUE KEY `e_mail` (`e_mail`);
+  ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `verification_sms`
+--
+ALTER TABLE `verification_sms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Friends`
+--
+ALTER TABLE `Friends`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `invitations`
+--
+ALTER TABLE `invitations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `Notifications`
+--
+ALTER TABLE `Notifications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `pets`
+--
+ALTER TABLE `pets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT for table `pets_pictures`
+--
+ALTER TABLE `pets_pictures`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+
+--
+-- AUTO_INCREMENT for table `sessions`
+--
+ALTER TABLE `sessions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT for table `verification_sms`
+--
+ALTER TABLE `verification_sms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
